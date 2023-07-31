9b2602e79c96b5014e72d639daca4e79e2d01536
yaroslav <yaroslavzakh.prog@gmail.com>
2023-07-31 23:22:41 +0300

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const routes = require("./core/routes/index");
 
@@ -6,6 +7,7 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/companies", routes.companyRoutes);
